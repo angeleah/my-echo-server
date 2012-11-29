b5ce3ff4370ca91cdb0edfb98ebb85608427592c
Angeleah Daidone <angeleah@angeleah.com>
2012-11-28 19:11:59 -0600

    Added conditions to the echo-server

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject echo_server "1.0.0-SNAPSHOT"
-  :description "echo server: write description"
+  :description "echo server in clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
 				[server-socket "1.0.0"]]
   :dev-dependencies [[speclj "2.1.1"]]
--- spec/echo_server/core-spec.clj
+++ spec/echo_server/core-spec.clj
@@ -1,7 +1,10 @@
-(ns chat_server.core-spec
-  (:use [speclj.core] [chat_server.core]))
+(ns echo_server.core-spec
+  (:use [speclj.core] 
+		[echo_server.core]))
 
 (describe "it should take the right action based on what is typed to the echo server"
-	(it "should say goodbye if you type quit"
-		(should= "goodbye" quit(quit)))
+	(it "should return 42 if you ask the meaning of life"
+		(should= 42 meaning-of-life))
 )
+
+;I don't get how to test this since nothing gets directly passed to echo server.
