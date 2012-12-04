f25f20c8f904be0a388a21af6ada18f83fb0193c
Angeleah Daidone <angeleah@angeleah.com>
2012-12-04 15:40:41 -0600

    Split the processing of the messages to a separate function.  Split the loop recur to a separate function. Add tests for the processing of messages.

---
+++ spec/echo_server/core_spec.clj
@@ -0,0 +1,19 @@
+(ns echo_server.core-spec
+  (:use [speclj.core] 
+		[echo_server.core]))
+
+(describe "it should be able to process the message based on what is typed to the echo server"
+
+	(it "should return '42' if you ask the meaning of life"
+		(should= "42" (process-message "what is the meaning of life?")))
+
+	(it "should return 'goodbye' if you type quit"
+		(should= "goodbye." (process-message "quit")))
+
+	(it "should return back what you typed if the input does not match 'what is the meaning of life' or 'quit'"
+		(should= "hello" (process-message "hello")))
+)
+
+(describe "it should be able to process a stream"
+
+)
--- src/echo_server/core.clj
+++ src/echo_server/core.clj
@@ -1,17 +1,33 @@
 (ns echo_server.core
 	(:use server.socket))
-(import '[java.io BufferedReader InputStreamReader OutputStreamWriter])
+(import '(java.io BufferedReader InputStreamReader OutputStreamWriter))
 
 (def port 1234)
+
+;(defn echo-server [in out]
+;	(binding [*in* (BufferedReader. (InputStreamReader. in))
+;			  *out* (OutputStreamWriter. out)]
+;		(loop [input (read-line)]
+;			(println (cond (= "quit" input) "goodbye."
+;				  (= "what is the meaning of life?" input) "42"
+;				 :else input))
+;			(if (not= "quit" input) (recur (read-line))))))
+
+
+(defn process-message [input]
+	(cond (= "quit" input) "goodbye."
+		  (= "what is the meaning of life?" input) "42"
+		 :else input))
+
+(defn process-stream []
+	(loop [input (read-line)]
+		(println (process-message input))
+		(if (not= "quit" input) (recur (read-line)))))
 
 (defn echo-server [in out]
 	(binding [*in* (BufferedReader. (InputStreamReader. in))
 			  *out* (OutputStreamWriter. out)]
-		(loop [input (read-line)]
-			(println (cond (= "quit" input) "goodbye."
-				  (= "what is the meaning of life?" input) "42"
-				 :else input))
-			(if (not= "quit" input) (recur (read-line))))))
+			(process-stream)))
 
 (defn -main []
 (create-server port echo-server))
